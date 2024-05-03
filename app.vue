ba90f978d21669f8e55bae144e4cbeba437a46d7
Whoistomiki <thomasyu@hotmail.fr>
2024-05-03 11:54:50 +0200

    Header + Footer done by Thomas

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+<<<<<<< HEAD
     <NuxtPage />
+=======
+    <Header />
+    <Footer />
+>>>>>>> 2512d68 (Header + Footer done by Thomas)
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NUFooter class="footer">
+      <div class="left-content">
+        <span>&copy; 2024 Pokédex en Nuxt fait par Thomas, Alexis, Roger et Killian</span>
+      </div>
+    </NUFooter>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  padding: 20px; 
+}
+
+.left-content {
+  font-size: 1rem;
+  text-align: center;
+}
+</style>
